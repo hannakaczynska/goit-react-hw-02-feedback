0637fab7c66a88831fd6a20fb1c2f0c208e98849
hannakaczynska <hannakacz13@gmail.com>
2023-05-18 19:52:31 +0200

    added

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,133 @@
+import { Component } from "react";
+
+class FeedbackOptions extends Component {
+static defaultProps = {
+    step: 1,
+};
+
+state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+}
+
+handleClick = e => {
+    const { name, value } = e.target;
+    const { step } = this.props;
+    this.setState({
+        [name]: +value + step  
+    })  
+}
+    
+    render() {
+    const { good, neutral, bad } = this.state
+        return <>
+            <h3>Please leave feedback</h3>
+            <div>
+                <button
+                    name="good"
+                    type="button"
+                    value={good}
+                    onClick={this.handleClick}>
+                    Good
+                </button>
+                <button
+                    name="neutral"
+                    type="button"
+                    value={neutral}
+                    onClick={this.handleClick}>
+                    Neutral
+                </button>
+                <button
+                    name="bad"
+                    type="button"
+                    value={bad}
+                    onClick={this.handleClick}>
+                    Bad
+                </button>
+            </div>
+        </>
+    }
+
+}
+
+export default FeedbackOptions;
+
+
+
+// import { Component } from "react";
+
+// class Feedback extends Component {
+// static defaultProps = {
+//     step: 1,
+// };
+
+// state = {
+//   good: 0,
+//   neutral: 0,
+//   bad: 0
+// }
+
+// handleClick = e => {
+//     const { name, value } = e.target;
+//     const { step } = this.props;
+//     this.setState({
+//         [name]: +value + step  
+//     })  
+//     }
+
+// countTotalFeedback = () => {
+//     const { good, neutral, bad } = this.state;
+//     const total = good + neutral + bad;
+//     return total
+//     }
+
+// countPositiveFeedbackPercentage = () => {
+//     const { good } = this.state;
+//     const total = this.countTotalFeedback();
+//     const positiveFeedback = ((good * 100) / total).toFixed(0);
+//     return positiveFeedback
+// }
+
+    
+//     render() {
+//     const { good, neutral, bad } = this.state
+//         return <>
+//             <h3>Please leave feedback</h3>
+//             <div>
+//                 <button
+//                     name="good"
+//                     type="button"
+//                     value={good}
+//                     onClick={this.handleClick}>
+//                     Good
+//                 </button>
+//                 <button
+//                     name="neutral"
+//                     type="button"
+//                     value={neutral}
+//                     onClick={this.handleClick}>
+//                     Neutral
+//                 </button>
+//                 <button
+//                     name="bad"
+//                     type="button"
+//                     value={bad}
+//                     onClick={this.handleClick}>
+//                     Bad
+//                 </button>
+//             </div>
+//             <h3>Statistics</h3>
+//             <ul>
+//                 <li>Good: { good }</li>
+//                 <li>Neutral: { neutral }</li>
+//                 <li>Bad: { bad }</li>
+//                 <li>Total: { this.countTotalFeedback() }</li>
+//                 <li>Positive feedback: { this.countPositiveFeedbackPercentage()}%</li>
+//             </ul>
+//         </>
+//     }
+
+// }
+
+// export default Feedback;
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class Section extends Component {
+
+    render() {
+        return <h3>Statistics</h3>
+}
+}
+
+export default Section
